148265549cd3946562307fc79407f8d069b1c922
kuncoro-0927 <khitanhesthik@gmail.com>
2024-11-06 12:20:07 +0700

    update Controllers

--- controllers/DaerahController.js
+++ controllers/DaerahController.js
@@ -1,7 +1,7 @@
-import { where } from "sequelize";
 import Daerah from "../models/DaerahModel.js";
 import path from "path";
 import fs from "fs";
+
 export const getDaerah = async (req, res) => {
   try {
     const response = await Daerah.findAll();
@@ -23,57 +23,56 @@ export const getDaerahById = async (req, res) => {
     console.log(error.message);
   }
 };
+
 export const saveDaerah = (req, res) => {
   if (req.files === null)
-    return res.status(400).json({ msg: "Tidak ada file yang di upload" });
+    return res.status(400).json({ msg: "No File Uploaded" });
   const name = req.body.title;
   const file = req.files.file;
   const fileSize = file.data.length;
   const ext = path.extname(file.name);
   const fileName = file.md5 + ext;
   const url = `${req.protocol}://${req.get("host")}/images/${fileName}`;
-  const allowedType = [".png", ".jpg", ".jpeg"];
+  const allowedType = [".png", ".jpg", ".jpeg", ".svg"];
 
   if (!allowedType.includes(ext.toLowerCase()))
-    return res.status(422).json({ msg: "Gambar tidak valid" });
+    return res.status(422).json({ msg: "Invalid Images" });
   if (fileSize > 5000000)
-    return res
-      .status(422)
-      .json({ msg: "Ukuran gambar tidak boleh lebih dari 5mb" });
+    return res.status(422).json({ msg: "Image must be less than 5 MB" });
 
   file.mv(`./public/images/${fileName}`, async (err) => {
     if (err) return res.status(500).json({ msg: err.message });
     try {
       await Daerah.create({ name: name, image: fileName, url: url });
-      res.status(201).json({ msg: "Daerah Berhasil di upload" });
+      res.status(201).json({ msg: "Daerah Created Successfuly" });
     } catch (error) {
       console.log(error.message);
     }
   });
 };
+
 export const updateDaerah = async (req, res) => {
   const daerah = await Daerah.findOne({
     where: {
       id: req.params.id,
     },
   });
-  if (!daerah) return res.status(404).json({ msg: "Data Tidak Ditemukan" });
+  if (!daerah) return res.status(404).json({ msg: "No Data Found" });
+
   let fileName = "";
   if (req.files === null) {
-    fileName = Daerah.image;
+    fileName = daerah.image;
   } else {
     const file = req.files.file;
     const fileSize = file.data.length;
     const ext = path.extname(file.name);
-    const fileName = file.md5 + ext;
-    const allowedType = [".png", ".jpg", ".jpeg"];
+    fileName = file.md5 + ext;
+    const allowedType = [".png", ".jpg", ".jpeg", ".svg"];
 
     if (!allowedType.includes(ext.toLowerCase()))
-      return res.status(422).json({ msg: "Gambar tidak valid" });
+      return res.status(422).json({ msg: "Invalid Images" });
     if (fileSize > 5000000)
-      return res
-        .status(422)
-        .json({ msg: "Ukuran gambar tidak boleh lebih dari 5mb" });
+      return res.status(422).json({ msg: "Image must be less than 5 MB" });
 
     const filepath = `./public/images/${daerah.image}`;
     fs.unlinkSync(filepath);
@@ -94,7 +93,7 @@ export const updateDaerah = async (req, res) => {
         },
       }
     );
-    res.status(200).json({ msg: "Daerah Berhasil diupdate" });
+    res.status(200).json({ msg: "Daerah Updated Successfuly" });
   } catch (error) {
     console.log(error.message);
   }
@@ -106,7 +105,8 @@ export const deleteDaerah = async (req, res) => {
       id: req.params.id,
     },
   });
-  if (!daerah) return res.status(404).json({ msg: "Data Tidak Ditemukan" });
+  if (!daerah) return res.status(404).json({ msg: "No Data Found" });
+
   try {
     const filepath = `./public/images/${daerah.image}`;
     fs.unlinkSync(filepath);
@@ -115,7 +115,7 @@ export const deleteDaerah = async (req, res) => {
         id: req.params.id,
       },
     });
-    res.status(200).json({ msg: "Daerah berhsail dihapus" });
+    res.status(200).json({ msg: "Daerah Deleted Successfuly" });
   } catch (error) {
     console.log(error.message);
   }
